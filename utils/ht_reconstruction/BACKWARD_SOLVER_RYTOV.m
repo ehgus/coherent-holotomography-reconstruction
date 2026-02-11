2849b672088b62bc711b38155696d631fa7a6cd7
Dohyeon Lee <dleh428@gmail.com>
2026-02-11 15:01:39 +0900

    more accurate z 3d mapping

--- utils/ht_reconstruction/BACKWARD_SOLVER_RYTOV.m
+++ utils/ht_reconstruction/BACKWARD_SOLVER_RYTOV.m
@@ -16,7 +16,7 @@ classdef BACKWARD_SOLVER_RYTOV < handle
             h.tomogram_size = params.tomogram_size;
             h.tomogram_resolution = params.tomogram_resolution;
         end
-        function [potential, Count]=solve(h,output_field,illum_k0)
+        function [potential, Count]=solve(h,output_field,illum_k0_pixel)
             % check fields and parameters
             assert(ndims(output_field) == 3, 'You need to provide the field with 3 dimenssion : dim1 x dim2 x illuminationnumber')
 
@@ -24,9 +24,9 @@ classdef BACKWARD_SOLVER_RYTOV < handle
             k_res = 1 ./ (h.tomogram_resolution .* h.tomogram_size);
             k0_nm = h.RI_bg / h.wavelength;
             kmax = h.NA / h.wavelength;
-            illum_k0_3d = zeros(3,size(illum_k0,2),'single');
-            illum_k0_3d(1:2,:) = illum_k0;
-            illum_k0_3d(3,:) = round(real(sqrt((k0_nm)^2 - (illum_k0(1,:)*k_res(1)).^2 - (illum_k0(2,:)*k_res(2)).^2))/k_res(3));
+            illum_k0_3d = zeros(3,size(illum_k0_pixel,2),'single');
+            illum_k0_3d(1:2,:) = illum_k0_pixel .* reshape(k_res(1:2),2,1);
+            illum_k0_3d(3,:) = real(sqrt((k0_nm)^2 - illum_k0_3d(1,:).^2 - illum_k0_3d(2,:).^2));
 
             % field-related information
             xsize_2d = size(output_field,1);
@@ -39,17 +39,18 @@ classdef BACKWARD_SOLVER_RYTOV < handle
             valid_2d_indices = find(NA_circle_2d);
             % Calculate kz (z-component of wavenumber)
             kz = sqrt(max(0,(k0_nm).^2 - (kxy_coords_2d).^2));
-            kz=kz(valid_2d_indices);
 
             % tomogram-related information
             xsize_3d = h.tomogram_size(1);
             ysize_3d = h.tomogram_size(2);
             zsize_3d = h.tomogram_size(3);
             % 3D Fourier space indices
-            [xcoords_3d, ycoords_3d] = ind2sub([xsize_2d, ysize_2d], valid_2d_indices);
-            xcoords_3d(xcoords_3d > xsize_2d/2) = xcoords_3d(xcoords_3d > xsize_2d/2) + (xsize_3d - xsize_2d);
-            ycoords_3d(ycoords_3d > ysize_2d/2) = ycoords_3d(ycoords_3d > ysize_2d/2) + (ysize_3d - ysize_2d);
-            zcoords_3d=round(kz/k_res(3))+1;
+            [xcoords_3d_pixel, ycoords_3d_pixel] = ind2sub([xsize_2d, ysize_2d], valid_2d_indices);
+            xcoords_3d_pixel(xcoords_3d_pixel > xsize_2d/2) = xcoords_3d_pixel(xcoords_3d_pixel > xsize_2d/2) + (xsize_3d - xsize_2d);
+            ycoords_3d_pixel(ycoords_3d_pixel > ysize_2d/2) = ycoords_3d_pixel(ycoords_3d_pixel > ysize_2d/2) + (ysize_3d - ysize_2d);
+            kxcoords_3d = (xcoords_3d_pixel - 1) * k_res(1);
+            kycoords_3d = (ycoords_3d_pixel - 1) * k_res(2);
+            kzcoords_3d = kz(valid_2d_indices);
 
             % Map 2D field to 3D Fourier space
             potential=zeros(xsize_3d,ysize_3d,zsize_3d,'single');
@@ -71,16 +72,12 @@ classdef BACKWARD_SOLVER_RYTOV < handle
             for i = 1:size(output_field,3)
                 % Acquire 2D Fourier space indices
                 % Rescale field
-                UsRytov_slice=circshift(UsRytov(:,:,i),[illum_k0_3d(1:2,i)]);
-                
-                UsRytov_slice=kz/1i.*UsRytov_slice(valid_2d_indices);% unit: (um^1) % kz is spatial frequency, so 2pi is multiplied for wave vector
+                UsRytov_slice=circshift(UsRytov(:,:,i),[illum_k0_pixel(1:2,i)]);
+                UsRytov_slice=kzcoords_3d/1i.*UsRytov_slice(valid_2d_indices);% unit: (um^1) % kz is spatial frequency, so 2pi is multiplied for wave vector
                 % Acquire 3D Fourier space indices
-                Fx_3d=xcoords_3d-illum_k0_3d(1,i);
-                Fy_3d=ycoords_3d-illum_k0_3d(2,i);
-                Fz_3d=zcoords_3d-illum_k0_3d(3,i);
-                Fx_3d=mod(Fx_3d-1,xsize_3d)+1;
-                Fy_3d=mod(Fy_3d-1,ysize_3d)+1;
-                Fz_3d=mod(Fz_3d-1,zsize_3d)+1;
+                Fx_3d=mod(round((kxcoords_3d-illum_k0_3d(1,i))/k_res(1)),xsize_3d)+1;
+                Fy_3d=mod(round((kycoords_3d-illum_k0_3d(2,i))/k_res(2)),ysize_3d)+1;
+                Fz_3d=mod(round((kzcoords_3d-illum_k0_3d(3,i))/k_res(3)),zsize_3d)+1;
                 Kzp_3d=sub2ind(size(Count),Fx_3d,Fy_3d,Fz_3d);
                 % Accumulate into 3D Fourier space
                 potential(Kzp_3d)=potential(Kzp_3d)+UsRytov_slice;
